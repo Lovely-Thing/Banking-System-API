3741bd85039b07f9c8d0895d0f3d749efe2490ae
Aditya Manikanth Rao <adityamanikanthrao@gmail.com>
2021-03-16 17:37:54 +0530

    bootstrapped project and added ts

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hey World!')
+})
+
+app.listen(port, ()=> console.log(`Server Up on http://localhost:${port}`));
