95f217c05e1ab0de02d53dbf32b76c18195554c1
freak2810 <adityamanikanthrao@gmail.com>
2021-04-06 00:23:47 +0530

    Added Paillier's ALgorithm

--- src/utils/security/Paillier.ts
+++ src/utils/security/Paillier.ts
@@ -1,76 +1,46 @@
 // All the variables are described as per this video:
 // https://www.youtube.com/watch?v=zsRmNT5Nc_0&t=616s
 
+// Implementation
+// https://en.wikipedia.org/wiki/Paillier_cryptosystem
+
 import {
-	prime,
-	lcm,
-	randBetween,
-	gcd,
-	modPow,
-	modInv,
-} from 'bigint-crypto-utils';
+	generateKeys,
+	cipherValue,
+	decipherValue,
+	multiplyMultipleCiphers,
+	multiplyTwoCiphers,
+} from './index';
 
 const BIT_SIZE = 128;
 
-function L(x: bigint, n: bigint) {
-	return BigInt((x - BigInt(1)) / BigInt(n));
-}
-
 // Choose two large prime numbers
-async function generatePrimeNumbers() {
-	const p = await prime(BIT_SIZE);
-	let q = await prime(BIT_SIZE);
+async function generator() {
+	const keys = await generateKeys(BIT_SIZE);
 
-	// p = BigInt(7);
-	// q = BigInt(11);
+	// console.log(keys);
 
-	while (p === q) {
-		q = await prime(BIT_SIZE);
-	}
+	const cipherText1 = await cipherValue(BigInt(10), keys.publicKey);
+	console.log(cipherText1);
+	// const cipherText2 = await cipherValue(BigInt(42), keys.publicKey);
+	// const cipherText3 = await cipherValue(BigInt(8), keys.publicKey);
 
-	const n: bigint = p * q;
-	const nSquared: bigint = n * n;
+	// const multipliedResult = multiplyTwoCiphers(cipherText1, cipherText2);
 
-	let g: bigint = randBetween(n * n);
-	// g = BigInt(5652);
+	// const multipliedResult = multiplyMultipleCiphers([
+	// 	cipherText1,
+	// 	cipherText2,
+	// 	cipherText3,
+	// ]);
 
-	while (gcd(g, nSquared) !== BigInt(1)) {
-		g = randBetween(n * n);
-	}
-
-	// console.log('p', p);
-	// console.log('q', q);
-	// console.log('n', n);
-	// console.log('n squared', nSquared);
-	// console.log('random Integer', g);
-
-	console.log('PUBLIC KEY', n, g);
-
-	//Encryption
-	const value = 270395;
-	const r: bigint = randBetween(n * n, BigInt(1));
-	// r = BigInt(23);
-
-	const c1 = modPow(g, value, nSquared);
-	const c2 = modPow(r, n, nSquared);
-
-	const c = (c1 * c2) % nSquared;
-
-	console.log('cypher: ', c);
-
-	// console.log('PRIVATE KEY', lambda, mu);
-
-	//Decryption
-	const lambda: bigint = lcm(p - BigInt(1), q - BigInt(1));
-	const l = L(modPow(g, lambda, nSquared), n);
-	const mu = modInv(l, n);
-
-	console.log('lambda', lambda);
-	console.log('l', l);
-	console.log('mu', mu);
-
-	const alpha = modPow(c, lambda, nSquared);
-	console.log('decipher: ', (L(alpha, n) * mu) % n);
+	// console.log(multipliedResult);
+	//
+	// const decipheredValue = await decipherValue(
+	// 	multipliedResult,
+	// 	keys.publicKey,
+	// 	keys.privateKey
+	// );
+	// console.log('decipher', decipheredValue);
 }
 
-generatePrimeNumbers();
+generator();
---
+++ src/utils/security/index.ts
@@ -0,0 +1,86 @@
+import {
+	gcd,
+	lcm,
+	modInv,
+	modPow,
+	prime,
+	randBetween,
+} from 'bigint-crypto-utils';
+
+const L = (x: bigint, n: bigint) => BigInt((x - BigInt(1)) / BigInt(n));
+
+export async function generatePrimeNumbers(BIT_SIZE: number) {
+	return await prime(BIT_SIZE);
+}
+
+interface PublicKey {
+	n: bigint;
+	g: bigint;
+}
+
+interface PrivateKey {
+	lambda: bigint;
+	mu: bigint;
+}
+
+export async function generateKeys(BIT_SIZE: number) {
+	const p = await generatePrimeNumbers(BIT_SIZE);
+	let q = await generatePrimeNumbers(BIT_SIZE);
+
+	while (p === q) {
+		q = await generatePrimeNumbers(BIT_SIZE);
+	}
+
+	const n: bigint = p * q;
+
+	let g: bigint = randBetween(n * n);
+	while (gcd(g, n * n) !== BigInt(1)) {
+		g = randBetween(n * n);
+	}
+
+	const lambda: bigint = lcm(p - BigInt(1), q - BigInt(1));
+	const l = L(modPow(g, lambda, n * n), n);
+	const mu = modInv(l, n);
+
+	return {
+		publicKey: { n, g },
+		privateKey: { lambda, mu },
+	};
+}
+
+export async function cipherValue(
+	value: number | bigint,
+	publicKey: PublicKey
+) {
+	const { n, g } = publicKey;
+
+	const nSquared = n * n;
+
+	const r: bigint = randBetween(n * n, BigInt(1));
+
+	const c1 = modPow(g, value, nSquared);
+	const c2 = modPow(r, n, nSquared);
+
+	return (c1 * c2) % nSquared;
+}
+
+export async function decipherValue(
+	cipher: bigint,
+	publicKey: PublicKey,
+	privateKey: PrivateKey
+) {
+	const { n } = publicKey;
+	const { lambda, mu } = privateKey;
+
+	const alpha = modPow(cipher, lambda, n * n);
+
+	return (L(alpha, n) * mu) % n;
+}
+
+export function multiplyTwoCiphers(cipher1: bigint, cipher2: bigint) {
+	return cipher1 * cipher2;
+}
+
+export function multiplyMultipleCiphers(ciphers: bigint[]) {
+	return ciphers.reduce((result, item) => result * item, BigInt(1));
+}
