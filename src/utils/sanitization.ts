cef0b8629b6f4bfcad239f3f36dd0175408d7ab9
freak2810 <adityamanikanthrao@gmail.com>
2021-04-26 21:42:11 +0530

    changed aadhaar verification to phone number

--- src/utils/sanitization.ts
+++ src/utils/sanitization.ts
@@ -3,7 +3,6 @@ import deleteProperty = Reflect.deleteProperty;
 
 export default function sanitizeLogin(customer: Customer) {
 	if (customer.hashedPassword) deleteProperty(customer, 'hashedPassword');
-	if (customer.aadhaar) deleteProperty(customer, 'aadhaar');
 	if (customer.address) deleteProperty(customer, 'address');
 	return customer;
 }
