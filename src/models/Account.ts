2d29ed7b208395e9fc9a600defc349c53abfdecd
freak2810 <adityamanikanthrao@gmail.com>
2021-05-02 18:03:06 +0530

    changed the format of ids for customer and account

--- src/models/Account.ts
+++ src/models/Account.ts
@@ -52,7 +52,7 @@ export class Account {
 	transactions!: Transaction[];
 
 	constructor(customer: Customer, publicKey: PublicKey, balance: bigint) {
-		this.accountNumber = new Randoma({ seed: 10 }).integer().toString();
+		this.accountNumber = new Randoma({ seed: 20 }).integer().toString();
 		this.customer = customer;
 		this.publicKey = {
 			n: publicKey?.n.toString() as string,
